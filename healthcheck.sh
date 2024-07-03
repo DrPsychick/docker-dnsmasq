88956f0c1b9c6ef0f379071db0dce2da47e2fbdc
DrPsychick <drpsychick@drsick.net>
2024-07-03 15:05:44 +0200

    cleanup and docs

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-if [ -z "$(ps |grep [d]nsmasq)" ]; then
+if [ -z "$(pgrep dnsmasq)" ]; then
   echo "no 'dnsmasq' process found!"
   exit 1
 fi
 
 if [ -z "$(netstat -nltu |grep \:53)" ]; then
   echo "no process listening on port 53!"
+  exit 1
+fi
+
+if [ -n "$KEEPALIVE_STATE" -a -z "$(pgrep keepalived)" ]; then
+  echo "keepalived configured but not running!"
   exit 1
 fi
